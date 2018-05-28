959c4ce52dafda473712ce1f00dc4691eb3a53b0
Anahi Costa <moanableu@gmail.com>
2018-05-28 11:03:32 -0400

    listView added to main

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.android.musicplayer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView mainView = findViewById(R.id.main_view);
 
     }
 }
