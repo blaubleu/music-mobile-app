a2ffdea0396a36a21443a05488b84fe8c0542e78
Anahi Costa <moanableu@gmail.com>
2018-05-28 17:29:42 -0400

    aesthetic setup intro

--- app/src/main/java/com/example/android/musicplayer/Album.java
+++ app/src/main/java/com/example/android/musicplayer/Album.java
@@ -3,6 +3,9 @@ package com.example.android.musicplayer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toolbar;
 
 public class Album extends AppCompatActivity {
     int position;
@@ -12,11 +15,12 @@ public class Album extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
 
-        // update action bar capabilities
+        // update action bar capabilities on top nav bar, remove once bottom menu is actionable!!
         getSupportActionBar().setTitle("Playing");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         /*Intent detailIntent = getIntent();
         position = detailIntent.getIntExtra("position", 0);*/
     }
+
 }
--- app/src/main/java/com/example/android/musicplayer/ArtistList.java
+++ app/src/main/java/com/example/android/musicplayer/ArtistList.java
@@ -9,16 +9,14 @@ public class ArtistList {
     ArtistList(){
 
         // array
+        artists.add(new Artist(R.drawable.indochine3, "Indochine", "J'ai demandé à la lune"));
         artists.add(new Artist(R.drawable.sodastereo2, "Soda Stereo", "Canción Animal"));
-        artists.add(new Artist(R.drawable.sodastereo2, "Soda Stereo", "Canción Animal"));
-        artists.add(new Artist(R.drawable.sodastereo2, "Soda Stereo", "Canción Animal"));
-        artists.add(new Artist(R.drawable.sodastereo2, "Soda Stereo", "Canción Animal"));
-        artists.add(new Artist(R.drawable.sodastereo2, "Soda Stereo", "Canción Animal"));
-        artists.add(new Artist(R.drawable.sodastereo2, "Soda Stereo", "Canción Animal"));
-        artists.add(new Artist(R.drawable.sodastereo2, "Soda Stereo", "Canción Animal"));
-        artists.add(new Artist(R.drawable.sodastereo2, "Soda Stereo", "Canción Animal"));
-        artists.add(new Artist(R.drawable.sodastereo2, "Soda Stereo", "Canción Animal"));
-        artists.add(new Artist(R.drawable.sodastereo2, "Soda Stereo", "Canción Animal"));
+        artists.add(new Artist(R.drawable.louiseattaque3, "Louise Attaque", "à plus tard crocodile"));
+        artists.add(new Artist(R.drawable.texas2, "Texas", "The Greatest Hits"));
+        artists.add(new Artist(R.drawable.melancholia2, "Melancholia", "Prague Philharmonic Orchestra"));
+        artists.add(new Artist(R.drawable.ninasimone2, "Nina Simone", "The Very Best of Nina Simone"));
+        artists.add(new Artist(R.drawable.amelie2, "Amélie", "Yann Tiersen"));
+        artists.add(new Artist(R.drawable.doctorstrange2, "Doctor Strange", "Michael Giacchino"));
 
     }
 }
