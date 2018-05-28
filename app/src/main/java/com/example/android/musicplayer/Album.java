5356bda57f5d1081f6903b610a03e8706112f599
Anahi Costa <moanableu@gmail.com>
2018-05-28 17:47:28 -0400

    bottom nav added to Album.onCreate

--- app/src/main/java/com/example/android/musicplayer/Album.java
+++ app/src/main/java/com/example/android/musicplayer/Album.java
@@ -1,10 +1,14 @@
 package com.example.android.musicplayer;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
 public class Album extends AppCompatActivity {
@@ -14,6 +18,15 @@ public class Album extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                //logic to switch song list goes here
+                return true;
+            }
+        });
 
         // update action bar capabilities on top nav bar, remove once bottom menu is actionable!!
         getSupportActionBar().setTitle("Playing");
