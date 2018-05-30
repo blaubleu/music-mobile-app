d84743a4439f05b65662b29b6add274b729d2178
Anahi Costa <moanableu@gmail.com>
2018-05-29 22:15:48 -0400

    testing parcelable implementation

--- app/src/main/java/com/example/android/musicplayer/Indochine.java
+++ app/src/main/java/com/example/android/musicplayer/Indochine.java
@@ -1,15 +1,50 @@
 package com.example.android.musicplayer;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
-public class Indochine {
+public class Indochine implements Parcelable {
+    private int cover;
+    private String artist;
+    private String song;
+
+    public Indochine(){}
 
     public static ArrayList<Songs> indochine = new ArrayList <>();
 
-    Indochine(){
-        indochine.add(new Songs(R.drawable.indochine3, "Indochine", "J'ai demandé à la lune"));
-        indochine.add(new Songs(R.drawable.indochine3, "Indochine", "Punker"));
-        indochine.add(new Songs(R.drawable.indochine3, "Indochine", "Glory Hole"));
+    Indochine(int cover, String artist, String song){
+        indochine.add(new Songs(R.drawable.indochine, "Indochine", "J'ai demandé à la lune"));
+        indochine.add(new Songs(R.drawable.indochine, "Indochine", "Punker"));
+        indochine.add(new Songs(R.drawable.indochine, "Indochine", "Glory Hole"));
     }
 
+    protected Indochine(Parcel in) {
+        cover = in.readInt();
+        artist = in.readString();
+        song = in.readString();
+    }
+
+    public static final Creator <Indochine> CREATOR = new Creator <Indochine>() {
+        @Override
+        public Indochine createFromParcel(Parcel in) {
+            return new Indochine(in);
+        }
+
+        @Override
+        public Indochine[] newArray(int size) {
+            return new Indochine[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
 }
